763bdef0b59ec720373cd3c8ae74c6b302ab4e68
Anish-2005 <123315153+Anish-2005@users.noreply.github.com>
2025-09-11 20:49:26 +0530

    Update page.tsx

--- src/app/atlas/page.tsx
+++ src/app/atlas/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = 'force-dynamic'
+
 import { useState, useRef, useEffect } from "react"
 import * as turf from "@turf/turf"
 import { STATES, DEFAULT_STATE } from "../../lib/regions"
